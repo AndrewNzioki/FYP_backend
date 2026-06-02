4cb0fa4fa5f0bc9fa021edc6d009d6e5919a9390
Andrew Nzioki <acnzioki5@gmail.com>
2026-06-02 22:26:42 +0300

    chore: Add proxy header support to Daphne in start script

--- digitaltwin_backend/start.sh
+++ digitaltwin_backend/start.sh
@@ -4,4 +4,4 @@ set -e
 echo "=== Starting Main Web Server (Daphne) ==="
 # We removed Celery, MQTT, and Migrations from here.
 # Docker Compose will handle them separately!
-daphne -b 0.0.0.0 -p ${PORT:-8000} --access-log /dev/null digitaltwin_backend.asgi:application
+daphne -b 0.0.0.0 -p ${PORT:-8000} --proxy-headers --access-log /dev/null digitaltwin_backend.asgi:application
